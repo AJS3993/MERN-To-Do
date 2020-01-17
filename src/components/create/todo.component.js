e16b23823413672beec78b801c31e61d7be5c66b
Andrew Smith <ajs3993@gmail.com>
2020-01-17 10:27:08 -0600

    create component functions

--- src/components/create/todo.component.js
+++ src/components/create/todo.component.js
@@ -13,9 +13,36 @@ export default class CreateTodo extends Component{
         }
     }
 
+onChangeTodoDescription(e){
+    this.setState({
+        todo_description: e.target.value
+    })
+} 
+
+onChangeTodoResponsible(e) {
+    this.setState({
+        todo_responsible: e.target.value
+    })
+}
+
+onChangeTodoPriority(e){
+    this.setState({
+        todo_priority: e.target.value
+    })
+} 
+
+onSubmit(e) {
+    e.preventDefault();
+
+    console.log('Form Submitted')
     
-    
-    
+    this.setState({
+        todo_description: '',
+        todo_responsible: '',
+        todo_priority: '',
+        todo_completed: false
+    })
+}
     
     render (){
     return(
